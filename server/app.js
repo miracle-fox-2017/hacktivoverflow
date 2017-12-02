95136c44745516ae74bdcad102c2e80b5e5b79a4
azhariemuhammad <azharie_muhammad@live.com>
2017-12-02 11:37:07 +0700

    app.js

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require ('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+app.use(cors())
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/hacktivOverflow')
+.then(() => console.log('db connection succesfull to hacktivoverflow'))
+.catch((err) => console.log(err))
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.listen(4000, function () {
+  console.log('im alive 4000')
+})
