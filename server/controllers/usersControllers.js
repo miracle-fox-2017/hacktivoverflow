996333d35db9b44a19f2cf66e37c9e8a02be4048
Nandira Paturohman <nandirasp@gmail.com>
2017-12-02 11:43:38 +0700

    add bcrypt and jwt

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -1,4 +1,7 @@
 const User = require('../models/userModel')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
 
 getAll = (req, res) => {
   User.find()
@@ -17,29 +20,67 @@ getOne = (req, res) => {
 }
 
 signup = (req, res) => {
-  User.create(req.body)
-  .then(newUser => {
-    res.send(newUser)
-  })
-  .catch(err => res.status(500).send(err))
+  bcrypt.hash(req.body.password, 10, function(error, hash) {
+    if(!error) {
+      req.body.password = hash
+      User.create(req.body)
+      .then(newUser => {
+        res.send(newUser)
+      })
+      .catch(err => res.status(500).send(err))
+    } else {
+      res.status(500).send(error)
+    }
+  });
 }
 
 login = (req, res) => {
-  res.send('test')
+  User.findOne({username: req.body.username})
+  .then(user => {
+    bcrypt.compare(req.body.password, user.password, function(err, response) {
+      if(!err) {
+        let payload = {
+          id: user._id,
+          username: user.username,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          email: user.email
+        }
+        jwt.sign(payload, process.env.JWT_SECRET, function(err, token) {
+          res.send({accesstoken: token})
+        })
+      } else {
+        res.status(401).send(err)
+      }
+    })
+  })
+  .catch(err => res.status(401).send(err))
 }
 
 update = (req, res) => {
   User.findById(req.params.id)
   .then(user => {
+    user.first_name = req.body.fisrt_name || user.first_name
+    user.last_name = req.body.last_name || user.last_name
+    user.email = req.body.email || user.email
 
+    user.save()
+    .then(userUpdated => {
+      res.send(userUpdated)
+    })
+    .catch(err => res.status(500).send(err))
   })
   .catch(err => res.status(500).send(err))
 }
 
 remove = (req, res) => {
-  User.remove({id: req.params.id})
+  User.findByIdAndRemove(req.params.id)
   .then(userDeleted => {
-    res.send(userDeleted)
+    let user = {
+      status: 'deleted',
+      data: userDeleted
+    }
+    res.send(user)
   })
   .catch(err => res.status(500).send(err))
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -6,7 +6,8 @@ const usersControllers = require('../controllers/usersControllers')
 router.get('/', usersControllers.getAll);
 router.get('/:id', usersControllers.getOne);
 router.post('/signup', usersControllers.signup);
-// router.put('/:id', usersControllers.update);
-// router.delete('/:id', usersControllers.remove);
+router.put('/:id', usersControllers.update);
+router.delete('/:id', usersControllers.remove);
+router.post('/login', usersControllers.login);
 
 module.exports = router;
