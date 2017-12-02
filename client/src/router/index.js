eb556c3e4514ebcfe23caecce406694f2276d4dd
aditReza <lete.note3@gmail.com>
2017-12-02 14:58:33 +0700

    setup client

---
+++ client/src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import LoginPage from '@/components/LoginPage'
+import RegisterPage from '@/components/RegisterPage'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/login',
+      component: LoginPage
+    },
+    {
+      path: '/register',
+      component: RegisterPage
+    }
+  ]
+})
