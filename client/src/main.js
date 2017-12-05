c32dc9879a03a021e55c114380604a76603ec628
Ahmad <matt.syahab@gmail.com>
2017-12-05 17:08:20 +0700

    change axios target to api

--- client/src/main.js
+++ client/src/main.js
@@ -7,10 +7,11 @@ import store from './vuex'
 import axios from 'axios'
 
 Vue.config.productionTip = false
+let url = "http://ec2-34-216-160-66.us-west-2.compute.amazonaws.com:3000"
+let local =  'http://localhost:3000'
 
 Vue.prototype.$http = axios.create({
-  // baseURL: 'http://localhost:3000'
-  baseURL: 'http://localhost:3000'
+  baseURL: url
 })
 
 /* eslint-disable no-new */
