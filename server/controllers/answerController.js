b1d4177231418acda9dddef82600774fc541b569
vbagustinus <vbagustinus@gmail.com>
2017-12-06 13:57:22 +0700

    fix mongo atlas

--- server/controllers/answerController.js
+++ server/controllers/answerController.js
@@ -1,5 +1,5 @@
-// const mongoose = require('mongoose').connect('mongodb://vbagustinus:anakjalanan@smartshop-shard-00-00-hibsb.mongodb.net:27017,smartshop-shard-00-01-hibsb.mongodb.net:27017,smartshop-shard-00-02-hibsb.mongodb.net:27017/todo?ssl=true&replicaSet=smartshop-shard-0&authSource=admin');
-const mongoose = require('mongoose').connect('mongodb://localhost/hacktivoverflow');
+const mongoose = require('mongoose').connect('mongodb://vbagustinus:anakjalanan@smartshop-shard-00-00-hibsb.mongodb.net:27017,smartshop-shard-00-01-hibsb.mongodb.net:27017,smartshop-shard-00-02-hibsb.mongodb.net:27017/hacktivoverflow?ssl=true&replicaSet=smartshop-shard-0&authSource=admin');
+// const mongoose = require('mongoose').connect('mongodb://localhost/hacktivoverflow');
 const Answer = require('../models/answerModel')
 const Question = require('../models/questionModel')
 const ObjectId = require('mongodb').ObjectId
